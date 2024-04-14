9b6666cb4cc1e059c2dce59a9e0c25e404dc9cd3
ShirefMohammed <shirefmo012345@gmail.com>
2024-04-14 15:27:32 +0200

    Uploading httpStatusText.js which provide and object which contains response is http status text - roles_list.js which provides an object which contains users roles list - sendResponse.js which send response on standard templete

---
+++ server/src/utils/httpStatusText.js
@@ -0,0 +1,13 @@
+const SUCCESS = "success";
+const FAIL = "fail";
+const ERROR = "error";
+const AccessTokenExpiredError = "AccessTokenExpiredError";
+const RefreshTokenExpiredError = "RefreshTokenExpiredError";
+
+module.exports = {
+  SUCCESS,
+  FAIL,
+  ERROR,
+  AccessTokenExpiredError,
+  RefreshTokenExpiredError,
+};
