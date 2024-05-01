2219bff55828f7ce4eb692ee1d69742ce36fbc40
ShirefMohammed <shirefmo012345@gmail.com>
2024-05-01 11:24:30 +0300

    Updating checkIfExistsInFirebase function

--- server/src/utils/checkIfExistsInFirebase.js
+++ server/src/utils/checkIfExistsInFirebase.js
@@ -1,9 +1,35 @@
+const { initializeApp } = require("firebase/app");
+const { getStorage, ref, getMetadata } = require("firebase/storage");
+const firebaseConfig = require("../config/firebaseConfig");
+
+initializeApp(firebaseConfig);
+
+const storage = getStorage();
+
 const checkIfExistsInFirebase = async (fileUrl) => {
   try {
-    const res = await fetch(fileUrl);
-    return res.status === 200;
-  } catch (err) {
-    return false;
+    const filenameStartIndex = fileUrl.indexOf("/o/") + 3;
+    const filenameEndIndex = fileUrl.indexOf("?");
+
+    if (
+      !fileUrl ||
+      !fileUrl.startsWith(
+        "https://firebasestorage.googleapis.com/v0/b/app-data-yo.appspot.com/o/"
+      ) ||
+      !fileUrl.startsWith("?alt=media", filenameEndIndex)
+    ) {
+      return false;
+    }
+
+    const filename = fileUrl.substring(filenameStartIndex, filenameEndIndex);
+
+    const fileRef = ref(storage, filename);
+
+    const metadata = await getMetadata(fileRef);
+
+    return metadata ? true : false;
+  } catch (error) {
+    console.error("Error checking file existence in Firebase Storage");
   }
 };
 
