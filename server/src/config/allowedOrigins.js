6de7523e472f096a02d8b226fc5dc9428ca0c956
ShirefMohammed <shirefmo012345@gmail.com>
2024-05-06 20:52:14 +0300

    Updating allowed origins

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://youtube-arlw.onrender.com",
+  "https://youtube-server-scf8.onrender.com",
+];
 
 module.exports = allowedOrigins;
